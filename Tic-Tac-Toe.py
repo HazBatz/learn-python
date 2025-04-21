f484c96b8b1afaf71524fec8dab6a6c63dace6a6
Eric T <erict12@duck.com>
2025-04-22 01:03:22 +0200

    init repo

---
+++ Tic-Tac-Toe.py
@@ -0,0 +1,13 @@
+# Tic-Tac-Toe
+
+import random
+
+def drawBoard(board):
+    # This function prnts out the board that it was passed
+
+    # "board" is a list of 10 strings representing the board (ignore index 0).
+    print
+    print
+    print
+    print
+    print
